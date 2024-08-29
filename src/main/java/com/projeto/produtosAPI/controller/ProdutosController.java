d4c60d0f666f70c086d447c95e133dcdafae5534
brandtLC <lucasbrandtcosta@gmail.com>
2024-08-29 02:09:36 -0300

    Add config inicial

---
+++ src/main/java/com/projeto/produtosAPI/controller/ProdutosController.java
@@ -0,0 +1,9 @@
+package com.projeto.produtosAPI.controller;
+
+import java.util.ArrayList;
+
+@RestController
+public class ProdutosController {
+    ArrayList<String> test = new ArrayList<String>();
+
+}
---
+++ src/test/java/com/projeto/produtosAPI/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.produtosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
